a908527842d45306af01e2081564cf0fe7199097
ChiuWeiChung <67630537+ChiuWeiChung@users.noreply.github.com>
2020-07-02 16:56:51 +0800

    Updates

---
+++ precache-manifest.77193ff243846e46b21d1df7fc6e51f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125656480dc2e00f5cb782bb4fde91c1",
+    "url": "/TicTacToe-React/index.html"
+  },
+  {
+    "revision": "dd56cdbfbc48f3c7448d",
+    "url": "/TicTacToe-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f8ba9499ca3bd336610",
+    "url": "/TicTacToe-React/static/js/2.3c44a1cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe-React/static/js/2.3c44a1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd56cdbfbc48f3c7448d",
+    "url": "/TicTacToe-React/static/js/main.5b27c964.chunk.js"
+  },
+  {
+    "revision": "4f452e3b954326d6e776",
+    "url": "/TicTacToe-React/static/js/runtime-main.a5a9458f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
